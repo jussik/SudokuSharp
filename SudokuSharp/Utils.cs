309904a4edff0d5d32a47f884315949e343d1f4e
Jussi Kosunen <deathcarrot@gmail.com>
2019-03-10 11:01:34 +0200

    popcnt via intrinsics

--- SudokuSharp/Utils.cs
+++ SudokuSharp/Utils.cs
@@ -1,4 +1,6 @@
-﻿namespace SudokuSharp
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace SudokuSharp
 {
     public static class Utils
     {
@@ -6,6 +8,9 @@
         {
             unchecked
             {
+                if (Popcnt.IsSupported)
+                    return (int)Popcnt.PopCount((uint)value);
+
                 int p = value - ((value >> 1) & 0x55555555);
                 p = (p & 0x33333333) + ((p >> 2) & 0x33333333);
                 return ((p + (p >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
